1698b92463ec67a0fb46b0ec5eecc9349c9f81f0
Your Name <you@example.com>
2024-03-21 17:26:54 -0600

    Verifying Git and project Setup

---
+++ 510K_Data_Manager/DataManagerMain.py
@@ -0,0 +1,11 @@
+# AuST
+# 3-21-24 
+# Created By : Jared Brinkman
+
+# Foundation for a 510K Data Manager. The Data Manager actively 
+# updates its database with the most current 510k Data. Based
+# on user input, it allows you to see various data represented 
+# by graphs, tables, etc... to make more informed decisions 
+# regarding future development efforts
+
+
